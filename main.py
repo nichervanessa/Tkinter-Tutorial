7aa31fb4fb504b711b79ba7d6e931b3f18ff4054
Nichervan Essa <nichervan.essj@outlook.com>
2024-05-29 19:09:18 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+from tkinter import * #in tkinter import everything
 
+root=Tk()
+root.title("GUI Software")
+root.geometry("1200x600")
+root.resizable(False,False)
+root.iconbitmap("folder\x.ico")
+
+root.mainloop()
